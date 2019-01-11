614e39e286a99f8c43e6628511d6c11c562b39b1
fummicc1-sub <fumiya989@gmail.com>
2019-01-11 20:12:58 +0900

    Initial Commit

---
+++ Julian_GameIntroductionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Julian_GameIntroductionApp
+//
+//  Created by Fumiya Tanaka on 2019/01/11.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
